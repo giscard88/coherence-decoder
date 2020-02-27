e8f4bf6da8228417efb297872a201909fcd37728
jung <giscard88@gmail.com>
2020-02-26 22:16:25 -0800

    update

--- find_pairs.py
+++ find_pairs.py
@@ -43,9 +43,14 @@ cwd=os.getcwd()
 target_dir=cwd+'/LRP/'+str(sid)
 os.chdir(target_dir)
 
+num_plane=int(4000/duration)*channel
+
+
 all_=defaultdict(list)
 
-for xin in [0,1,2,3]:
+cut_off_=defaultdict(list)
+cut_num=100
+for xin in [0,1,2,3]: # target classes 
     target_str='H_tr'+str(duration)+'-'+str(channel)+'_10_'+str(xin)+'*.pt' # _10_ is always there and thus redundant. Need to be removed later. 
  
     file_list=glob(target_str)
@@ -55,24 +60,49 @@ for xin in [0,1,2,3]:
         data=data.flatten()
         args_=np.argsort(data)
         args_=np.flip(args_,0)
-        all_[str(xin)].extend(args_[:5])
-   
+        mean=np.mean(data)
+        std=np.std(data)
+        cut_off=mean+5.0
+        for ii in range(cut_num):
+            temp=args_[ii]
+            if temp>cut_off:
+                all_[str(xin)].append(temp)
+        
+ 
 final={}  
-for xin in [0,1,2,3]:
+for xin in [0,1,2,3]: # target classes 
     temp=all_[str(xin)]
-    count=np.zeros(16*44*44)
+    count=np.zeros(num_plane*44*44)
     for yin in temp:
         count[yin]=count[yin]+1
     temp_arg=np.argsort(count)
     temp_arg=np.flip(temp_arg,0)
     
-    final[str(xin)]=temp_arg[:5]
+    final[str(xin)]=temp_arg[:cut_num]
     
 
 
-print (final)
+freq=defaultdict(list)
 
+for xin in final:
+    temp=final[xin]
+    for t in temp:
+        freq[t].extend(xin)
+
+print (freq)
+scan=defaultdict(list)
+
+for xin in freq:
+    temp_=''
+    labels=freq[xin]
+    for l in labels:
+
+        temp_=temp_+str(l)
     
+    
+    scan[temp_].append(xin)
+
+print (scan)
 
 
 
